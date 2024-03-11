8248a99fc0a7ce14eb6188d70b6f3eb972445ab3
zhdaniukivan <iz2819815@gmail.com>
2024-03-11 19:44:11 +0300

    init commit

--- site_to_check_junior/settings.py
+++ site_to_check_junior/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test_page',
+    'user_page',
+    'reqruter_page',
 ]
 
 MIDDLEWARE = [
