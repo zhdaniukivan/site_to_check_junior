f46068bc99a6e5e184795295bf93c05fd96d48f8
zhdaniukivan <iz2819815@gmail.com>
2024-03-13 20:29:10 +0300

    created models

--- test_page/models.py
+++ test_page/models.py
@@ -1,13 +1,38 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-class Post(models.Model):
+
+class Profile(models.Model):
+
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateTimeField(null=True)
+    profile_creation_time = models.DateTimeField(default=timezone.now())
+    last_visit_time = models.DateTimeField(blank=True)
+    gender = models.CharField(max_length=1, choices=Gender.choices, blank=True)
+
+
+class Question(models.Model):
 
     class Status(models.TextChoices):
-        DRAFT = 'DR', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = 'M', 'Moderation'
+        PUBLISHED = 'P', 'Published'
+
+    class Language(models.TextChoices):
+        Python = 'P', 'Python'
+        JavaScript = 'JS', 'JavaScript'
+        Java = 'J', 'Java'
+        Typescript = 'T', 'Typescript'
+        C = 'C#', 'C#'
+        PHP = 'PHP', 'PHP'
 
 
+    language = models.CharField(max_length=30, choices=Language.choices)
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -15,12 +40,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    added_by_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField()
 
-    class Meta:
-        ordering = ['-publish']
-        index = [
-            models.Index(fields=['publish']),
-        ]
 
     def __str__(self):
         return self.title
