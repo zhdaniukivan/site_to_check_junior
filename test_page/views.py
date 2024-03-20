a2243d6efb1f070f75ed1e69198d8327db8ff472
zhdaniukivan <iz2819815@gmail.com>
2024-03-20 16:20:06 +0300

    created urls

--- test_page/views.py
+++ test_page/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('page test')
+
+
+def categories(request):
+    return HttpResponse('<hi>Test to Categories')
